d667046c77e72076881b0dad916b77be0d285292
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-05-13 22:33:18 +0200

    Add a dumb test to check whether reports are generated and linked.

---
+++ src/test/java/com/github/mfriedenhagen/flattenmavenpluginoverridessitexml/MyTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.mfriedenhagen.flattenmavenpluginoverridessitexml;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Mirko Friedenhagen
+ */
+public class MyTest {
+
+    @Test
+    public void shouldPass() {
+        Assert.assertTrue(true);
+    }
+}
